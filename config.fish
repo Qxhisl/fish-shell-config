20a702daaf3cf5d61e9b2a55f2cfed84602a6fc9
Qxhisl <b1508vms@gmail.com>
2021-10-20 03:47:06 +0900

    first commit

---
+++ config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_color_command	00ffff  #コマンド:明るい水色
+set fish_color_param    ff5fff  #コマンドの引数:明るい紫
+set fish_color_quote    ffff00  #クォートを含む文字列全体:明るい黄色
